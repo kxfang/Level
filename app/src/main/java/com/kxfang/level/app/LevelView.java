cd203789dda1194fe8836d32346f6312bafe7da7
Kevin Fang <kfang@kfang.com>
2014-03-07 16:21:24 -0500

    Minor refactoring

--- app/src/main/java/com/kxfang/level/app/LevelView.java
+++ app/src/main/java/com/kxfang/level/app/LevelView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.hardware.SensorManager;
 import android.util.AttributeSet;
@@ -15,11 +17,22 @@ public class LevelView extends View {
   private float mTheta;
   private float mRotation;
 
+  // Paint objects to optimize onDraw
+  Paint mTextPaint;
+
   /**
    * Constructor to be used to inflate the view
    */
   public LevelView(Context context, AttributeSet attrs) {
     super(context, attrs);
+
+    // TODO: refactor into xml
+    mTextPaint = new Paint();
+    mTextPaint.setColor(Color.WHITE);
+    mTextPaint.setAntiAlias(true);
+    mTextPaint.setTextAlign(Paint.Align.CENTER);
+    mTextPaint.setTextSize(250.0f);
+    mTextPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.XOR));
   }
 
   public void render(float[] values) {
@@ -36,18 +49,17 @@ public class LevelView extends View {
   @Override
   protected void onDraw(Canvas c) {
     super.onDraw(c);
-    c.rotate(mRotation, getCentreX(), getCenterY());
     c.drawColor(Color.BLACK);
     c.saveLayer(null, null, Canvas.MATRIX_SAVE_FLAG);
-    Paint p = new Paint();
-    p.setColor(Color.WHITE);
-    p.setAntiAlias(true);
-    p.setTextAlign(Paint.Align.CENTER);
-    p.setTextSize(250.0f);
+    c.rotate(mRotation, getCentreX(), getCenterY());
     String text = "" + (int) mTheta;
     Rect textBounds = new Rect();
-    p.getTextBounds(text, 0, text.length(), textBounds);
-    c.drawText(text, getCentreX(), getCenterY() + (textBounds.bottom - textBounds.top)/2, p);
+    mTextPaint.getTextBounds(text, 0, text.length(), textBounds);
+    c.drawText(
+        text,
+        getCentreX(),
+        getCenterY() + (textBounds.bottom - textBounds.top)/2,
+        mTextPaint);
     c.restore();
   }
 
