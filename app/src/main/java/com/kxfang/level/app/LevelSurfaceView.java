ced1127bae92c6d8b83566425b1c1713a1498240
Kevin Fang <kfang@kfang.com>
2014-03-07 13:55:43 -0500

    Added rotating degree indicator

--- app/src/main/java/com/kxfang/level/app/LevelSurfaceView.java
+++ app/src/main/java/com/kxfang/level/app/LevelSurfaceView.java
@@ -4,8 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.hardware.SensorManager;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.SurfaceView;
 
 public class LevelSurfaceView extends SurfaceView {
@@ -18,11 +19,33 @@ public class LevelSurfaceView extends SurfaceView {
   }
 
   public void render(float[] values) {
+
+    float theta = (float) Math.toDegrees(Math.acos(values[2] / SensorManager.GRAVITY_EARTH));
+    float rotation = (float) Math.toDegrees(Math.atan(values[0] / values[1]));
+    if ((values[0] < 0 && values[1] < 0) || (values[0] >= 0 && values[1] < 0)) {
+      rotation += 180;
+    }
+
     Canvas c = getHolder().lockCanvas();
+    c.rotate(rotation, getCentreX(), getCenterY());
     c.drawColor(Color.BLACK);
     Paint p = new Paint();
     p.setColor(Color.WHITE);
-    c.drawRect(0.0f, 0.0f, Math.abs(values[0]) * 100, Math.abs(values[1]) * 100, p);
+    p.setAntiAlias(true);
+    p.setTextAlign(Paint.Align.CENTER);
+    p.setTextSize(250.0f);
+    String text = "" + (int) theta;
+    Rect textBounds = new Rect();
+    p.getTextBounds(text, 0, text.length(), textBounds);
+    c.drawText(text, getCentreX(), getCenterY() + (textBounds.bottom - textBounds.top)/2, p);
     getHolder().unlockCanvasAndPost(c);
+  }
+
+  private float getCentreX() {
+    return getRight() / 2;
+  }
+
+  private float getCenterY() {
+    return getBottom() / 2;
   }
 }
