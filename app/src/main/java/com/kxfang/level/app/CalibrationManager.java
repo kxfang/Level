6bafc4df10640096ecc39d1bd4d14ffeb2c9b2a0
Kevin Fang <kfang@kfang.com>
2014-03-18 19:35:03 -0400

    Add methods for storing horizontal calibration

--- app/src/main/java/com/kxfang/level/app/CalibrationManager.java
+++ app/src/main/java/com/kxfang/level/app/CalibrationManager.java
@@ -9,6 +9,7 @@ import android.content.SharedPreferences;
 public class CalibrationManager {
   private static final String PREFERENCES_FILENAME = "calibration.prefs";
   private static final String FLAT_PREFIX = "flat_";
+  private static final String HORIZONTAL_PREFIX = "horizon_"
   private static final CalibrationManager INSTANCE = new CalibrationManager();
 
   private CalibrationManager() { }
@@ -17,19 +18,37 @@ public class CalibrationManager {
     return INSTANCE;
   }
 
-  public void storeFlatCalibration(Context context, float[] values) {
+  private void storeCalibration(Context context, float[] values, boolean horizontal) {
+    String prefix = horizontal ? HORIZONTAL_PREFIX : FLAT_PREFIX;
     SharedPreferences.Editor prefs = context.getSharedPreferences(PREFERENCES_FILENAME, 0).edit();
     for (int i = 0; i < values.length; i++) {
-      prefs.putFloat(FLAT_PREFIX + i, values[i]);
+      prefs.putFloat(prefix + i, values[i]);
     }
     prefs.commit();
   }
 
-  public void loadFlatCalibration(Context context, float[] values) {
+  private void loadCalibration(Context context, float[] values, boolean horizontal) {
+    String prefix = horizontal ? HORIZONTAL_PREFIX : FLAT_PREFIX;
     SharedPreferences prefs = context.getSharedPreferences(PREFERENCES_FILENAME, 0);
     for (int i = 0; i < values.length; i++) {
-      values[i] = prefs.getFloat(FLAT_PREFIX + i, 0);
+      values[i] = prefs.getFloat(prefix + i, 0);
     }
+  }
+
+  public void storeHorizontalCalibration(Context context, float[] values) {
+    storeCalibration(context, values, true);
+  }
+
+  public void loadHorizontalCalibration(Context context, float[] values) {
+    loadCalibration(context, values, true);
+  }
+
+  public void storeFlatCalibration(Context context, float[] values) {
+    storeCalibration(context, values, false);
+  }
+
+  public void loadFlatCalibration(Context context, float[] values) {
+    loadCalibration(context, values, false);
   }
 
   public void clearCalibration(Context context) {
